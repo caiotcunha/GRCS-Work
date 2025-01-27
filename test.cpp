0786511b9cafa7cc6d28ec92ce7edfdddbea893d
Caio Teles <caiotelescunha2001@gmail.com>
2025-01-27 13:20:57 -0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <cryptopp/sha.h>
+#include <cryptopp/hex.h>
+#include <time.h>
+
+//#include "cryptoPAN.h"
+#include "Tcrypto.h"
+#include "DSPAN.h"
+
+double time1, timedif;
+
+int main() {
+    time1 = (double) clock();
+    time1 = time1 / CLOCKS_PER_SEC; 
+    using namespace CryptoPP;
+    using namespace std;
+
+    cout << "main started" << endl;
+    string key = "boojahyoo3vaeToong0Eijee7Ahz3yee";
+
+    string line;
+    ifstream file("./database/ipv4.txt");
+
+    //CryptoPan *cp = new CryptoPan(key);
+    //TCrypto *tcp = new TCrypto(key,1);
+    TCrypto *dscp = new TCrypto(key,21);
+
+    if (file.is_open()){
+        while (getline(file,line)){
+            //cp->anonymize("boojahyoo3vaeToong0Eijee7Ahz3yee",line);
+            //tcp->anonymize(line);
+            dscp->anonymize(line);
+        }
+        file.close();
+    }
+
+    //delete cp;
+    //delete tcp;
+
+    timedif = ( ((double) clock()) / CLOCKS_PER_SEC) - time1;
+
+    cout << timedif << endl;
+
+    return 0;
+}
